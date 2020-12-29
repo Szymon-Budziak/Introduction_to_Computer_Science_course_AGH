b830163111a0304d4d4156d7352a0792fc0d830d
szymon-budziak <szymonbudziak@gmail.com>
2020-12-29 15:34:48 +0100

    Solutions from Section 1

--- Section_1/section_1-exercise_4.py
+++
@@ -1,9 +0,0 @@
-number = int(input("Enter a number: "))
-starting_number = 1
-i = 0
-result = 1
-while result <= number:
-    starting_number += 2
-    result += starting_number
-    i += 1
-print("Element of this number is:", i)
