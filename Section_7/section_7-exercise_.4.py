fe00a06172020a15879a2c4f3a2337758ca10653
szymon-budziak <szymonbudziak@gmail.com>
2021-03-01 19:53:32 +0100

    Solutions from Section 7 (part 1)

--- Section_7/section_7-exercise_.4.py
+++ Section_7/section_7-exercise_.4.py
@@ -4,17 +4,15 @@ class Node:
         self.next = next_node
 
 
-def reverse_list(a):
-    if a is None:
-        return None
-    if a.next is None:
+def reverse_list(L):
+    if L is None:
         return None
     p = None
-    q = a
-    r = a.next
-    while q is not None:
-        q.next = p
-        p = q
-        q = r
-        if r is not None:
-            r = r.next
+    q = L.next
+    while L is not None:
+        L.next = p
+        p = L
+        L = q
+        if q is not None:
+            q = q.next
+    return p
