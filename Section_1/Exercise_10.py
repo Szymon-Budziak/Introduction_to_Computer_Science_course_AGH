f2942872be168c8dbde33d44df5f64df11cd4476
szymonbudziak@gmail.com <szymonbudziak@gmail.com>
2021-11-14 16:08:15 +0100

    Code fix

--- Section_1/Exercise_10.py
+++ Section_1/Exercise_10.py
@@ -1,13 +1,11 @@
 """
 Napisać program wyszukujący liczby doskonałe mniejsze od miliona.
 """
-number = 1000
-result = 0
-while number > 1:
-    for i in range(1, number + 1):
-        if number % i == 0:
-            result += i
-            if result == number:
-                print("Number", number, "is perfect number.")
-    result = 0
-    number -= 1
+number = 1000000
+for i in range(1, number + 1):
+    summary = 0
+    for j in range(1, (i // 2) + 1):
+        if i % j == 0:
+            summary += j
+    if summary == i:
+        print(summary)
